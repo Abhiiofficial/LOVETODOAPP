7d0a7d764f4dea3071c1c917b6205e905f32aaaa
Abhishek <you@example.com>
2023-05-22 15:59:43 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://todo-ai6z.vercel.app/'
   }));
 app.use(express.json());
 app.use('/api/v1/user', userRoute)
